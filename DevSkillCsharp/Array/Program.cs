16bcc9c7cc02b5c9276d427930f07f75e5227970
monir <monir.bu18@gmail.com>
2023-01-17 17:22:06 +0600

    Create Project Basic C# DevSkills

---
+++ DevSkillCsharp/Array/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // int[] arr = { 22, 69, 82 };
+            // string[] arr = new string[2];
+            int[] arr2 = new int[5];
+            int[] arr3 = new int[5];
+            arr2[0] = 1;
+            arr2[1] = 2;
+            arr2[2] = 3;
+            arr2[3] = 6;
+            arr2[4] = 11;
+            //arr2[5] = 11;
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+
+            Console.WriteLine("End of arr2");
+
+            int j =0;
+
+            for(int i = 0; i < arr2.Length; i++)
+            {
+                arr3[j] = arr2[i];
+                j++;
+            }
+
+
+            for (int k = 0; k < arr2.Length; k++)
+            {
+                Console.WriteLine(arr3[k]);
+            }
+          
+        }
+    }
+}
---
+++ DevSkillCsharp/DataTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+
+        static unsafe void Main(string[] args)
+        {
+            //int age=0;
+            //Console.WriteLine(age);
+
+            //int a = 10;
+            //Console.WriteLine(a);
+            //int b = a;
+
+            //Console.WriteLine(b);
+            //b = 20;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //string txt = "Hello";
+            //Console.WriteLine(txt);
+            //string ss = txt;
+            //Console.WriteLine(ss);
+
+            string[] arr = new string[2];
+            arr[0] = "1";
+            arr[1] = "22";
+            Console.WriteLine(arr[1]);
+
+            int a = 10;
+            int * ad = &a;
+            *ad = 20;
+
+            //Console.WriteLine((int)&val);
+
+    
+
+            Console.WriteLine(*ad);
+            Console.WriteLine(a);
+
+        }
+    }
+}
